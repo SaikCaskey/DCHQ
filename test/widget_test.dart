73dfd167378887866c500913d5f0834fcc600545
SaikCaskey <floreat.bangoria@gmail.com>
2022-05-01 13:04:12 +0200

    Update widget_test.dart
    update the test to check only if the app can be built in a test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,31 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:alex_ui/alex_ui.dart';
+import 'package:alex_ui/theme/style_type.dart';
+import 'package:dchq/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dchq/main.dart';
+Future<void> main() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  final lightTheme = await AppThemeFactory.create(
+    AppThemeType.normal,
+    isDark: false,
+  );
+  final darkTheme = await AppThemeFactory.create(
+    AppThemeType.normal,
+    isDark: true,
+  );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Can build test app with alex_ui theme',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(App(
+      lightThemeData: lightTheme,
+      darkThemeData: darkTheme,
+      themeMode: ThemeMode.dark,
+    ));
+    await tester.pumpAndSettle();
   });
 }
