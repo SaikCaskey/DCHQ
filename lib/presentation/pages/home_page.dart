826fb257706ec41191016c002cd1b95962fc181a
SaikCaskey <floreat.bangoria@gmail.com>
2022-05-01 11:42:49 +0200

    Remove the success handler widget
    this was resetting the counter value when >= 7 which I didn't want

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -2,7 +2,6 @@ import 'package:dchq/unitedstates/timer_state/timer_state_event.dart';
 import 'package:flutter/material.dart';
 import 'package:washington/washington.dart';
 
-import '../../unitedstates/counter_state/counter_event.dart';
 import '../../unitedstates/counter_state/counter_state.dart';
 import '../../unitedstates/counter_state/counter_state_event.dart';
 import '../../unitedstates/timer_state/timer_state_provider.dart';
@@ -35,68 +34,60 @@ class MyHomePage extends StatelessWidget {
                       content: Text('Timer limit has been reached!')));
                 }
               },
-              child: StateListener<CounterState, int>.success(
-                successListener: (context, state) {
-                  // We can trigger events based on state changes
-                  if (state.value >= 7) {
-                    Washington.instance.dispatch(CounterResetPressed());
-                  }
-                },
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const Text(
-                        'Current value:',
-                      ),
-                      StateBuilder<CounterState, int>(
-                        builder: (context, state) {
-                          final String text;
-                          if (state.hasError) {
-                            text = state.error.toString();
-                          } else if (state.isLoading) {
-                            text = 'Loading...';
-                          } else {
-                            text = state.value.toString();
-                          }
-                          return Text(
-                            text,
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline4
-                                ?.copyWith(
-                                    color: state.hasError ? Colors.red : null),
-                          );
-                        },
-                      ),
-                      const Text(
-                        'Timer value:',
-                      ),
-                      StateBuilder<TimerState, int>(
-                        builder: (context, state) {
-                          final String text;
-                          if (state.hasError) {
-                            text = state.error.toString();
-                          } else if (state.isLoading) {
-                            text = state.value.toString();
-                          } else {
-                            text = state.value.toString();
-                          }
-                          return Text(
-                            text,
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline4
-                                ?.copyWith(
-                                    color: state.hasError ? Colors.red : null),
-                          );
-                        },
-                      ),
-                      const SizedBox(
-                        height: 32,
-                      ),
-                    ],
-                  ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'Current value:',
+                    ),
+                    StateBuilder<CounterState, int>(
+                      builder: (context, state) {
+                        final String text;
+                        if (state.hasError) {
+                          text = state.error.toString();
+                        } else if (state.isLoading) {
+                          text = 'Loading...';
+                        } else {
+                          text = state.value.toString();
+                        }
+                        return Text(
+                          text,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline4
+                              ?.copyWith(
+                                  color: state.hasError ? Colors.red : null),
+                        );
+                      },
+                    ),
+                    const Text(
+                      'Timer value:',
+                    ),
+                    StateBuilder<TimerState, int>(
+                      builder: (context, state) {
+                        final String text;
+                        if (state.hasError) {
+                          text = state.error.toString();
+                        } else if (state.isLoading) {
+                          text = state.value.toString();
+                        } else {
+                          text = state.value.toString();
+                        }
+                        return Text(
+                          text,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline4
+                              ?.copyWith(
+                                  color: state.hasError ? Colors.red : null),
+                        );
+                      },
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                  ],
                 ),
               ),
             ),
